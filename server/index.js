185c3c23c9e597396dfac63d5502e4e339a2b158
Arbel Amram <arbelamram@gmail.com>
2024-07-04 15:30:23 +0300

    DISPLAY received data in a DESC-ORDER table

--- server/index.js
+++ server/index.js
@@ -1,26 +1,17 @@
-// const express = require('express');
-// const connectDB = require('./config/db');
-
-// const app = express();
-
-// // Connect to database
-// connectDB();
-
-// app.use(express.json());
-
-// // Define Routes
-// app.use('/', require('./routes/index')); // GET - redirect short url to original url
-// app.use('/api/urls', require('./routes/urls')); // GET ALL or POST
-
-// const PORT = 5000;
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"] })
-})
+// Connect to database
+connectDB();
 
-app.listen(5000, () => {console.log("server started on port 5000")})
+app.use(express.json());
+
+// Define Routes
+app.use('/', require('./routes/index')); // GET - redirect short url to original url
+app.use('/api/url', require('./routes/url')); // GET ALL or POST
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
