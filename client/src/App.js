2a893b65baa3395e3987755f8aeefbe44fb03aad
Arbel Amram <arbelamram@gmail.com>
2024-07-04 12:01:07 +0300

    REACT displays EXPRESS data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const connectDB = require('./config/db');
+// const express = require('express');
+// const connectDB = require('./config/db');
 
+// const app = express();
+
+// // Connect to database
+// connectDB();
+
+// app.use(express.json());
+
+// // Define Routes
+// app.use('/', require('./routes/index')); // GET - redirect short url to original url
+// app.use('/api/urls', require('./routes/urls')); // GET ALL or POST
+
+// const PORT = 5000;
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const express = require('express');
 const app = express();
 
-// Connect to database
-connectDB();
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"] })
+})
 
-app.use(express.json());
-
-// Define Routes
-app.use('/', require('./routes/index')); // GET - redirect short url to original url
-app.use('/api/urls', require('./routes/urls')); // GET ALL or POST
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => {console.log("server started on port 5000")})
